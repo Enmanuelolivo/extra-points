12eb61d50226522a4ef87fc36a62cd92b24dc8ad
Enmanuelolivo <45018826+Enmanuelolivo@users.noreply.github.com>
2019-09-20 15:25:58 -0400

    initial commit

---
+++ ExtraPoints/ExtraPoints/ExtraPoints.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "310a1b2b-db95-4e1b-8deb-3df7faab1d30",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
---
+++ ExtraPoints/ExtraPoints/ExtraPoints/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExtraPoints
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private Dictionary<long, SKPath> temporaryPaths = new Dictionary<long, SKPath>();
+        private List<SKPath> paths = new List<SKPath>();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PaintSample_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+
+
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+            canvas.Clear(SKColors.Bisque);
+
+            var stroke = new SKPaint
+            {
+                Color = SKColors.DarkOrange,
+                StrokeWidth = 5,
+                Style = SKPaintStyle.Stroke
+            };
+
+            foreach (var touchPath in paths)
+            {
+                canvas.DrawPath(touchPath, stroke);
+            }
+        }
+
+
+
+        private void PaintSample_Touch(object sender, SKTouchEventArgs e)
+        {
+            switch (e.ActionType)
+            {
+                case SKTouchAction.Pressed:
+                    var p = new SKPath();
+                    p.MoveTo(e.Location);
+                    temporaryPaths[e.Id] = p;
+                    break;
+                case SKTouchAction.Moved:
+                    if (e.InContact)
+                        temporaryPaths[e.Id].LineTo(e.Location);
+                    break;
+                case SKTouchAction.Released:
+                    paths.Add(temporaryPaths[e.Id]);
+                    temporaryPaths.Remove(e.Id);
+                    break;
+            }
+
+            e.Handled = true;
+
+            // update the UI on the screen
+            ((SKCanvasView)sender).InvalidateSurface();
+        }
+    }
+    }
+
+
